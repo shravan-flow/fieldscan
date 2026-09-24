e3312f9b6e4ea6d0c284ea00fbaecc2071319e65
shravan-flow <shravanaiyappa@gmail.com>
2026-09-24 17:40:47 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fieldscan-v1';
+const CACHE = 'fieldscan-v2';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
